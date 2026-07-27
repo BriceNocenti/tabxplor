5a1e3544e6077035d02b0fe39042c4e034b99df7
Brice Nocenti <brice.nocenti@protonmail.com>
2026-07-27 20:49:28 +0200

    before release

--- tests/testthat/_problems/test-export-89.R
+++
@@ -1,8 +0,0 @@
-# Extracted from test-export.R:89
-
-# prequel ----------------------------------------------------------------------
-t_row <- tab(forcats::gss_cat, race, marital, pct = "row", color = c("diff", "ratio"))
-
-# test -------------------------------------------------------------------------
-merged <- tab(forcats::gss_cat, c(race, relig), marital, pct = "row")
-k_both <- as.character(tab_export(merged, "kable", engine = "html", css = FALSE))
--- tests/testthat/_problems/test-golden-119.R
+++
@@ -1,5 +0,0 @@
-# Extracted from test-golden.R:119
-
-# 
-# Failed to extract test: 
-# Failed to find test at line 119.
